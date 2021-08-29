38ea1a995fb9e886983d488008798c3d2ff1f104
Mohammad Afandy <mohammad.afandy@weekendinc.com>
2021-08-29 22:21:00 +0700

    remove duplicate folder

--- src/Components/Banner/BannerWrapper.tsx
+++
@@ -1,9 +0,0 @@
-const BannerWrapper: React.FC = ({ children }) => {
-  return (
-    <div className="bg-banner-gradient h-full flex justify-center items-center p-2 sm:p-10">
-      {children}
-    </div>
-  );
-};
-
-export default BannerWrapper;
